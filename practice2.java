f198cc71806624be7c3160daa63e879728f0bf5d
mahdi <zarbi.mahdi0101@gmail.com>
2025-04-08 15:05:22 +0330

    practice2

---
+++ practice2.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class practice2 {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter your phrase: ");
+        String result="";
+        String word=scan.nextLine();
+        for(int i=0;i<word.length();i++){
+            char w=word.charAt(i);
+            if (result.indexOf(String.valueOf(w)) == -1){
+                result+=w;
+            }
+        }
+        System.out.println(result);
+        scan.close();
+    }
+    
+}
