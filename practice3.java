534aa9cd7e715083ea25398827e85f6c07252466
mahdi <zarbi.mahdi0101@gmail.com>
2025-04-10 14:27:12 +0330

    Update practice3.java

--- practice3.java
+++ practice3.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class practice3 {
     public static void main(String[] args) {
-        
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an expression with brackets: ");
+        String s = scanner.nextLine();
+        scanner.close();
     
+        int len;
+        do {
+            len = s.length();
+            s = s.replace("()", "");
+            s = s.replace("{}", "");
+            s = s.replace("[]", "");
+        } while (len != s.length());
+    
+        if (s.isEmpty()) {
+             System.out.println("The expression is balanced.");
+        } else {
+            System.out.println("The expression is not balanced.");
+        }
+        }
 }
+    
+
